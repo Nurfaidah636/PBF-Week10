5e91bb010a370032d8cecab0422f805975792797
Nurfaidah636 <70933551+Nurfaidah636@users.noreply.github.com>
2021-04-19 03:06:44 +0800

    Add files via upload

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBRVSg1NV71fG6L-nMWe1odQDEsARj6bbU",
+    authDomain: "fir-15d47.firebaseapp.com",
+    projectId: "fir-15d47",
+    storageBucket: "fir-15d47.appspot.com",
+    messagingSenderId: "480019419053",
+    appId: "1:480019419053:web:554bc2f4112d19ae7bdb24",
+    measurementId: "G-6HLT4C999Z"
+};
